34bc5337108b53429a3215cf22cd580e9b9837a8
Kok-Wei Bong <bongkokwei@gmail.com>
2025-12-11 16:16:42 +1100

    created init file

---
+++ src/luna_ova/__init__.py
@@ -0,0 +1,10 @@
+"""
+Luna OVA (Optical Vector Analyser) Python Interface
+
+A Python package for controlling Luna OVA optical vector analysers via TCP/IP.
+"""
+
+from .luna_ova import LunaOVA
+
+__version__ = "0.1.0"
+__all__ = ["LunaOVA"]
